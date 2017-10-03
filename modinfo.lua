fe81844e42e4ae1f8175a297e8185e538e99e7ab
folknor <folk@folk.wtf>
2017-10-03 17:18:59 +0200

    Initial upload, mostly garbage.

---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "bombsaway",
+	name = "Bombs Away, Major!",
+	tags = { "Gameplay", "Spaceships" },
+	picture = false,
+	supported_version = "1.8.*",
+	remote_file_id = false,
+	--readme = "README.md",
+	--steambb = "steam.bbcode",
+	-- Files and folders added in exclude will not be copied over to the Paradox Interactive
+	-- local mod folder when you run `stlrel --steam`.
+	--exclude = {
+	--	"source", ".git", "steam.bbcode", "README.md"
+	--}
+}
